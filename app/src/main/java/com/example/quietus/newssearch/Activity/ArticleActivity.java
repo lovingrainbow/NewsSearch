ed0f9b9067f908666ba6caa254c193cff7d6374b
= <lwydctl@gmail.com>
2017-02-26 18:01:02 +0800

    NYTimesSearch basic function finish: User can enter a search query that will display a grid of news articles using the thumbnail and headline from the New York Times Search API. (3 points) User can click on "filter" icon which allows selection of advanced search options to filter articles. (3 points) An example of a query with filters (begin_date, sort, and news_desk) applied can be found here. Full details of the API can be found on this article search README. User can configure advanced search filters such as: (points included above) Begin Date (using a date picker) Sort order (oldest or newest) using a spinner dropdown News desk values (Arts, Fashion & Style, Sports) using checkboxes Subsequent searches will have any filters applied to the search results. (1 point) User can tap on any article in results to view the contents in an embedded browser. (2 points) User can scroll down "infinitely" to continue loading more news articles. The maximum number of articles is limited by the API search. (1 point) Use the ActionBar SearchView or custom layout as the query box instead of an EditText. (1 point)
    Stretch: Use Parcelable instead of Serializable using the popular Parceler library. (1 point)
    Stretch: Replace all icon drawables and other static image assets with vector drawables where appropriate. (1 point)
    User can share a link to their friends or email it to themselves. (1 point)
    Stretch: Replace the embedded WebView for viewing news articles with a Chrome Custom Tab using a custom action icon for sharing. (2 points)
    new add:
    Robust error handling, check if internet is available, handle error cases, network failures. (1 point)

--- app/src/main/java/com/example/quietus/newssearch/Activity/ArticleActivity.java
+++ app/src/main/java/com/example/quietus/newssearch/Activity/ArticleActivity.java
@@ -1,9 +1,14 @@
 package com.example.quietus.newssearch.Activity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,6 +20,8 @@ import org.parceler.Parcels;
 
 public class ArticleActivity extends AppCompatActivity {
 
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
     private WebView wvArticle;
     Article article;
     @Override
@@ -22,11 +29,8 @@ public class ArticleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
         article = (Article) Parcels.unwrap(getIntent().getParcelableExtra("Article"));
-
-
-        //setView();
+        setView();
     }
-
     private void setView(){
         wvArticle = (WebView)findViewById(R.id.wvArticle);
         String url = article.getWebUrl();
@@ -39,5 +43,16 @@ public class ArticleActivity extends AppCompatActivity {
         });
         wvArticle.loadUrl(url);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_article_actionbar, menu);
+        MenuItem item = menu.findItem(R.id.mnuShare);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, article.getWebUrl());
+        miShareAction.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/quietus/newssearch/Activity/FilterActivity.java
+++ app/src/main/java/com/example/quietus/newssearch/Activity/FilterActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
